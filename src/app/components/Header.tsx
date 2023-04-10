a360251435564f0f35e795b30bd886b965d430c0
jmussardev <jmussard.dev@gmail.com>
2023-04-10 14:18:55 +0200

    Menu OK

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,47 @@
+"use client";
+
+import { useState } from "react";
 import NavBar from "./Navbar";
+import cross from "./../../../public/icons/cross.png";
+import Image from "next/image";
+import menu from "./../../../public/icons/menu.png";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <div className="z-10 sticky top-0   w-full h-[6rem] bg-white border-2 border-rose-500 <##> sm:static sm:h-full sm:w-1/4 sm:p-6 ">
+      <div className="z-10 sticky top-0   w-full h-[6rem] bg-white border-2 border-rose-500  <##> sm:static sm:h-full sm:w-1/4 sm:p-6 ">
         <div className="absolute w-full z-50 flex justify-between pb-3 items-center bg-white border border-yellow-400 -- sm:static sm:w-full">
-          <div className=" ml-2 mt-2 text-right w-[70px]  bg-gray-300 font-bold text-3xl">
+          <div className=" ml-2 mt-2 text-right w-[70px]  bg-black text-white font-bold text-3xl">
             <span className="block">DO</span>
             <span className="block pr-[3px] ">.IT</span>
           </div>
-          <div className="h-14 w-14 mr-2 mt-2 rounded-full bg-purple-400 "></div>
+
+          <div className="h-14 w-14 mr-2 mt-2 rounded-full bg-purple-400 hidden sm:block  sm:ml-16"></div>
+          <button
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            <div className="h-14 w-14 mr-2 mt-2 rounded-full  sm:hidden">
+              {open ? (
+                <Image src={cross} alt="" />
+              ) : (
+                <Image src={menu} alt="" />
+              )}
+            </div>
+          </button>
         </div>
-        <NavBar />
+        <div
+          className={`block ${
+            open ? "translate-y-[6rem]" : "-translate-y-[10rem]"
+          } transition-all ease-in-out sm:hidden`}
+        >
+          <NavBar setOpen={setOpen} />
+        </div>
+        <div className="hidden  sm:block">
+          <NavBar setOpen={setOpen} />
+        </div>
       </div>
     </>
   );
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,20 +1,49 @@
 import Link from "next/link";
+import { Dispatch, SetStateAction } from "react";
 
-export default function NavBar() {
+export default function NavBar({
+  setOpen,
+}: {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}) {
   return (
-    <div className=" w-full font-bold text-lg text-center  -- sm:static sm:text-left ">
+    <div className=" w-full font-bold text-lg text-center bg-white border-b-2 border-black  -- sm:static sm:text-left ">
       <ul className=" sm:border-black border-t-[3px] mb-10 pt-4">
         <li>
-          <Link href={"/"}>Today</Link>
+          <Link href={"/"}>
+            {" "}
+            <button
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              Today
+            </button>{" "}
+          </Link>
         </li>
         <li>
-          <Link href={"/my-week"}> My week</Link>
+          <Link href={"/my-week"}>
+            {" "}
+            <button
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              My week
+            </button>
+          </Link>
         </li>
         <li>
-          <Link href={"/archived"}> Archived</Link>
-        </li>
-        <li>
-          <Link href={"/settings"}> Settings</Link>
+          <Link href={"/archived"}>
+            {" "}
+            <button
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              Archived
+            </button>
+          </Link>
         </li>
       </ul>
       <div className="pt-4 mb-5 border-black border-t-[3px]  ">
