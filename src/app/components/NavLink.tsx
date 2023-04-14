853a6ca73436cc5e2d352ca8f410f9241eb5a601
jmussardev <jmussard.dev@gmail.com>
2023-04-14 12:16:52 +0200

    navlink fix
    isOldTask fix

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -13,37 +13,56 @@ export interface NavLinkType {
 }
 
 function NavLink({
-  href = "",
+  href,
   exact = false,
   children = null,
 
   ...props
 }: NavLinkType) {
   const { getDay, today } = getDate();
-  const tDayNum = getDay(today());
-  const url = usePathname();
-  const dayPage = href.split("/")[href.split("/").length - 1];
-  const isOldTask = () => {
-    if (dayPage < tDayNum) return true;
-    else return false;
-  };
+  const tDayNum = parseInt(getDay(today()));
+  //url => url dans le navigateur //
+  //href=> url du lien cliqué
+  let url = usePathname();
+  const dayPage = parseInt(href.split("/")[href.split("/").length - 1]);
 
+  //pour etre consider comme is old le lien le numero du jour de la semaine contenu dan href doit etre < au # du jour d'aujourdhui
+  const isOldTask = () => {
+    if (dayPage < tDayNum) {
+      return true;
+    } else return false;
+  };
+  console.log("tDayNum>>> " + tDayNum);
+  console.log("dayPage>>> " + dayPage);
+  console.log("href>>> " + href);
+  console.log("href>>> " + href);
+
+  //pour etre active le url doit correspondre au href
   const isActive = exact ? url === href : false;
   console.log(">>>>>>>>>>>" + isOldTask());
+  console.log(">>>>>>>>>>>" + isActive);
 
   //   bg-black text-white
   return (
+    //on veut  dans href passer le lien a cliqué
     <Link href={href}>
       <div
-        className={` ${
-          isOldTask() ? "bg-gray-300" : 0
-        } flex justify-center items-center w-[3.5rem] xsm:w-[7.6rem] h-[6rem] pt-4  -translate-y-4 border-l-2 border-b-2 border-r-2 border-black rounded-b-md  font-bold ## ${
+        className={` 
+        ${isOldTask() ? " isOld text-black " : ""} 
+         flex justify-center items-center w-[3.5rem] xsm:w-[7.6rem] h-[6rem] pt-4  -translate-y-4 
+        ${
+          isActive && isOldTask() === false
+            ? "-translate-y-[5px] bg-black text-white"
+            : ""
+        }  
+        ${isOldTask() && isActive ? "-translate-y-[5px] text-black " : ""} 
+         border-l-2 border-b-2 border-r-2 border-black rounded-b-md  font-bold ## 
+        ${
           isActive
             ? ""
-            : "bg-white hover:-translate-y-1  hover:bg-black hover:text-white"
-        }  ## sm:w-[5rem] sm:h-[6rem] "     ${
-          isActive ? "-translate-y-1 bg-black text-white" : ""
-        }`}
+            : "bg-white hover:-translate-y-[5px]  hover:bg-black hover:text-white"
+        } 
+         ## sm:w-[5rem] sm:h-[6rem]  `}
       >
         {children}
       </div>
