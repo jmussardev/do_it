0b1e9e4bb8f00f1c004a861a6cba87c3b4edb4d1
jmussardev <jmussard.dev@gmail.com>
2023-04-27 17:22:28 +0200

    ;

--- src/app/components/DotLoading.tsx
+++ src/app/components/DotLoading.tsx
@@ -2,11 +2,16 @@
 import { useTheme } from "next-themes";
 
 export default function DotLoading() {
-  const { theme } = useTheme();
+  const { systemTheme, theme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
     <div className="  h-full w-full flex justify-center mt-24">
-      <div className={theme === "dark" ? "lds-ellipsis--dark" : "lds-ellipsis"}>
+      <div
+        className={
+          currentTheme === "dark" ? "lds-ellipsis--dark" : "lds-ellipsis"
+        }
+      >
         <div></div>
         <div></div>
         <div></div>
